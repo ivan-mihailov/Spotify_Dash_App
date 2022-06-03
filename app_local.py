ebed452909b278b86a2131131f2ab2f0e13bf61f
ivan-mihailov <ivan.i.mihailov@gmail.com>
2022-06-03 16:10:10 +0200

    Refactor code to remove full loading of 1 mln+ dataframes and only load the input and recommended songs

--- app_local.py
+++ app_local.py
@@ -9,12 +9,6 @@ from dash.dependencies import Input, Output
 
 """Create and configure an instance of the Dash application."""
 
-# Load training dataframe to get wrangled songs
-df_train = pd.read_csv('data/df_train.zip', compression='zip')
-
-# Load unwrangled dataset to match the song.
-df_rec_lookup = pd.read_csv('data/df_rec_lookup.zip', compression='zip')
-
 # Load pickled model and recommendations lookup dataframe
 knn_loader = joblib.load('ml/knn_model.joblib')
 
@@ -29,66 +23,6 @@ def style_c():
     layout_style = {'display': 'inline-block', 'margin': '0 auto',
                     'padding': '20px'}
     return layout_style
-
-
-# Function to generate radar graph for song selected by user
-def user_song_fig(df, i):
-    # Generate user song and artist names
-    user_song_input = df.columns[i]
-    user_song_name = df_rec_lookup.loc[user_song_input, 'name']
-    user_song_artist = df_rec_lookup.loc[user_song_input, 'artists']. \
-        replace("'", "").strip("[]")
-    fig_user_song = go.Figure()
-    fig_user_song.add_trace(go.Scatterpolar(r=df.iloc[:, i],
-                                            theta=df.iloc[:, 0],
-                                            fill='toself'))
-    fig_user_song.update_layout(polar=dict(radialaxis=dict(visible=True,
-                                                           range=[0, 1])),
-                                title={'text': f'You have selected <br>'
-                                               f'{user_song_name} <br> by '
-                                               f'{user_song_artist}',
-                                       'xanchor': 'center', 'yanchor': 'top',
-                                       'y': 0.95, 'x': 0.5}, showlegend=False)
-    return fig_user_song
-
-
-# Function to generate overlay radar graph for songs recommended by model
-def rec_song_fig(df, i):
-    # Generate recommended songs and artists names
-    rec_song_i_input = df.columns[i]
-    rec_song_i_name = df_rec_lookup.loc[rec_song_i_input, 'name']
-    rec_song_i_artist = df_rec_lookup.loc[rec_song_i_input, 'artists']. \
-        replace("'", "").strip("[]")
-    p = inflect.engine()
-
-    fig_rec_song_i = go.Figure()
-    # Generate radar graph for the user selected song
-    fig_rec_song_i.add_trace(go.Scatterpolar(
-        r=df.iloc[:, 1],
-        theta=df.iloc[:, 0],
-        fill='toself',
-        name='User Selected Song'
-    ))
-    # Generate the overlay radar graph for each of the recommended songs
-    fig_rec_song_i.add_trace(go.Scatterpolar(
-        r=df.iloc[:, i],
-        theta=df.iloc[:, 0],
-        fill='toself',
-        name=f'{p.ordinal(i - 1)} Recommended Song'
-    ))
-    # Generate graph title and legend
-    fig_rec_song_i.update_layout(
-        polar=dict(
-            radialaxis=dict(
-                visible=True,
-                range=[0, 1]
-            )),
-        title={'text': f'Your {p.ordinal(i - 1)} recommended song is <br>'
-                       f'{rec_song_i_name} <br> by {rec_song_i_artist}',
-               'xanchor': 'center', 'yanchor': 'top', 'y': 0.95, 'x': 0.5},
-        showlegend=True
-    )
-    return fig_rec_song_i
 
 
 player_base_path = "https://open.spotify.com/embed/track/"
@@ -229,7 +163,7 @@ app.layout = html.Div([
                        'margin': '100 px auto',
                        })
         ]),
-    ])
+])
 
 
 @app.callback(
@@ -243,21 +177,110 @@ app.layout = html.Div([
     Output(component_id='rec_song_3_player', component_property='children'),
     Input(component_id='user_song', component_property='value'))
 def update_plot(input_song):
-    doc = df_train.iloc[[input_song]]
+    # Load columns for training and recommendation lookup dataframes
+    cols_train = pd.read_csv('data/df_train.zip', compression='zip',
+                             header=0).columns
+    cols_rec = pd.read_csv('data/df_rec_lookup.zip', compression='zip',
+                           header=0).columns
+
+    # Load training dataframe to get wrangled input song
+    df_train = pd.read_csv('data/df_train.zip', compression='zip',
+                           names=cols_train,
+                           skiprows=lambda x: x not in [input_song + 1])
 
     # Query Using K-Nearest Neighbors Model
-    __, neigh_index = knn_loader.kneighbors(doc)
+    __, neigh_index = knn_loader.kneighbors(df_train)
+    list_rec = list(neigh_index[0][:4])
 
     # Generate dataframe for visualizations
-    df_viz = df_train.iloc[neigh_index[0][:4]]
+    wrangled_song_list = []
+    for i in list_rec:
+        wrangled_song_list.append(pd.read_csv('data/df_train.zip',
+                                              compression='zip', header=None,
+                                              skiprows=lambda x: x not in [i + 1]))
+
+    df_viz = pd.concat(wrangled_song_list, ignore_index=True)
+    df_viz.columns = [cols_train]
+
     # Drop age columns as unnecessary for visualization
     df_viz = df_viz[df_viz.columns.drop(list(df_viz.filter(regex='age')))]
+
     # Transpose dataframe for use in graph generating function
     df_viz_transposed = df_viz.transpose().reset_index()
 
+    # Load unwrangled dataset to match the songs for mini-Spotify player
+    unwrangled_song_list = []
+    for i in list_rec:
+        unwrangled_song_list.append(pd.read_csv('data/df_rec_lookup.zip',
+                                                compression='zip', header=None,
+                                                skiprows=lambda x: x not in [i + 1]))
+
+    df_rec_lookup = pd.concat(unwrangled_song_list, ignore_index=True)
+    df_rec_lookup.columns = [cols_rec]
+
+    # Load song IDs for mini-Spotify Players
     song_id = []
-    for i in neigh_index[0][0:4]:
-        song_id.append(df_rec_lookup['id'][i])
+    for i in range(0, 4):
+        song_id.append(df_rec_lookup.loc[i]['id'])
+
+    # Function to generate radar graph for song selected by user
+    def user_song_fig(df, j):
+        # Generate user song and artist names
+        user_song_input = df.columns[j]
+        user_song_name = df_rec_lookup.loc[user_song_input]['name']
+        user_song_artist = df_rec_lookup.loc[user_song_input]['artists']
+        user_song_artist = user_song_artist.replace("'", "").strip("[]")
+
+        fig_user_song = go.Figure()
+        fig_user_song.add_trace(go.Scatterpolar(r=df.iloc[:, j],
+                                                theta=df.iloc[:, 0],
+                                                fill='toself'))
+        fig_user_song.update_layout(polar=dict(radialaxis=dict(visible=True,
+                                                               range=[0, 1])),
+                                    title={'text': f'You have selected <br>'
+                                                   f'{user_song_name} <br> by '
+                                                   f'{user_song_artist}',
+                                           'xanchor': 'center', 'yanchor': 'top',
+                                           'y': 0.95, 'x': 0.5}, showlegend=False)
+        return fig_user_song
+
+    # Function to generate overlay radar graph for songs recommended by model
+    def rec_song_fig(df, k):
+        # Generate recommended songs and artists names
+        rec_song_k_input = df.columns[k]
+        rec_song_k_name = df_rec_lookup.loc[rec_song_k_input]['name']
+        rec_song_k_artist = df_rec_lookup.loc[rec_song_k_input]['artists']
+        rec_song_k_artist = rec_song_k_artist.replace("'", "").strip("[]")
+        p = inflect.engine()
+
+        fig_rec_song_k = go.Figure()
+        # Generate radar graph for the user selected song
+        fig_rec_song_k.add_trace(go.Scatterpolar(
+            r=df.iloc[:, 1],
+            theta=df.iloc[:, 0],
+            fill='toself',
+            name='User Selected Song'
+        ))
+        # Generate the overlay radar graph for each of the recommended songs
+        fig_rec_song_k.add_trace(go.Scatterpolar(
+            r=df.iloc[:, k],
+            theta=df.iloc[:, 0],
+            fill='toself',
+            name=f'{p.ordinal(k - 1)} Recommended Song'
+        ))
+        # Generate graph title and legend
+        fig_rec_song_k.update_layout(
+            polar=dict(
+                radialaxis=dict(
+                    visible=True,
+                    range=[0, 1]
+                )),
+            title={'text': f'Your {p.ordinal(k - 1)} recommended song is <br>'
+                           f'{rec_song_k_name} <br> by {rec_song_k_artist}',
+                   'xanchor': 'center', 'yanchor': 'top', 'y': 0.95, 'x': 0.5},
+            showlegend=True
+        )
+        return fig_rec_song_k
 
     # Generate graph and mini-Spotify-player for song selected by user
     fig_user_song = user_song_fig(df_viz_transposed, 1)
